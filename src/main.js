d57398d2aa8bd250451fe4f0df5125319e9c9afd
Paritosh Nichat <paritoshnichat@gmail.com>
2022-09-16 08:58:46 +0530

    created a express server to listen to requests

--- src/main.js
+++ src/main.js
@@ -1,12 +1,38 @@
 // For more information, see https://crawlee.dev/
 import { PuppeteerCrawler, ProxyConfiguration } from 'crawlee';
 import { router } from './routes.js';
+import express from 'express';
 
-const startUrls = ['https://crawlee.dev'];
+const app = express();
+const hostname = 'puppeteer-crawler.vercel.app';
+// const hostname = 'localhost';
+const port = 3000;
 
-const crawler = new PuppeteerCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+  
+app.post('/crawl', async (req, res) => {
+  console.log(req.body.startUrl);
+  const startUrls = [req.body.startUrl];
+
+    const crawler = new PuppeteerCrawler({
+        // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+        requestHandler: router,
+    });
+
+    await crawler.run(startUrls);
+    res.sendStatus(200);
 });
 
-await crawler.run(startUrls);
+app.listen(port, hostname, () => {
+    console.log(`Server running at https://${hostname}:${port}/`)
+})
